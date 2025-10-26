c721e2aa958e1435deffb8bf4c3e826403be14f8
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-10-26 14:11:33 -0300

    create first migration on knex

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,36 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
-  {
-    files: ['**/*.ts', '**/*.js'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json'
-      }
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin
-    },
-    rules: {
-      'semi': ['error', 'always'], // Força o uso de ;
-      'indent': ['error', 'tab'] // Força tabs para indentação
-    }
-  }
+	{
+		// Configuração para arquivos TypeScript
+		files: ['**/*.ts', '*.ts'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				project: './tsconfig.json'
+			}
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin
+		},
+		rules: {
+			'semi': ['error', 'always'],
+			'indent': ['error', 'tab']
+		}
+	},
+	{
+		// Configuração para arquivos JavaScript (SEM o project)
+		files: ['**/*.js', '*.js'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module'
+			// Note: SEM parser e SEM project aqui
+		},
+		rules: {
+			'semi': ['error', 'always'],
+			'indent': ['error', 'tab']
+		}
+	}
 ];
