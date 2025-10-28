68432b87e84c9619c1b5c74354a1773b128cdb54
Gabriel Camurça <gabriel@brtech.dev>
2025-10-28 08:24:45 -0300

    fix: fastify server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app.js";
 import { env } from "./env/index.js";
 
 app.listen({
-	port: env.PORT
+	port: env.PORT,
+	host: env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
 }).then(() => {
 	console.log('HTTP Server Running');
 });
