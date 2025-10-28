fd528cf27f14b1770ad440d7f6eccde9c363c100
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-10-27 23:40:07 -0300

    categorize tests

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { test, beforeAll, afterAll, describe } from 'vitest';
+import request from 'supertest';
+import { app } from '../src/app.js';
+
+describe('Transactions routes', async () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	test('user can create a new transaction', async () => {
+		const response = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New Transaction',
+				amount: 500,
+				type: 'credit'
+			})
+			.expect(201);
+	});
+});
