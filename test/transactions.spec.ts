5c3b85d91795522ffa55d8572cd847140c8ab5b0
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-10-27 23:57:31 -0300

    e2e list transactions test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest';
+import { it, beforeAll, afterAll, describe, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app.js';
 
@@ -11,8 +11,8 @@ describe('Transactions routes', async () => {
 		await app.close();
 	});
 
-	test('user can create a new transaction', async () => {
-		const response = await request(app.server)
+	it('should be able to create a new transaction', async () => {
+		await request(app.server)
 			.post('/transactions')
 			.send({
 				title: 'New Transaction',
@@ -20,5 +20,33 @@ describe('Transactions routes', async () => {
 				type: 'credit'
 			})
 			.expect(201);
+	});
+
+	it('should be able to list all transactions', async () => {
+		const createTransactionResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New Transaction',
+				amount: 500,
+				type: 'credit'
+			});
+		
+		const cookies = createTransactionResponse.get('Set-Cookie');
+
+		if (!cookies) {
+			throw new Error('No cookies received');
+		}
+
+		const listTransactionsResponse = await request(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies)
+			.expect(200);
+
+		expect(listTransactionsResponse.body.transactions).toEqual([
+			expect.objectContaining({
+				title: 'New Transaction',
+				amount: 500,
+			})
+		]);
 	});
 });
